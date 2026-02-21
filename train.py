49501892787721ffdfef6f541c7eeb8ce76456a6
Maksiu <hiimpela@gmail.com>
2026-02-21 11:00:58 +0100

    Updated filenames and deleted deprecated files

---
+++ train.py
@@ -0,0 +1,18 @@
+import mnist_loader
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper(expand = True)
+import network2
+net = network2.network([784, 800, 10], cost=network2.cross_entropy)
+net.sgd(training_data = training_data, 
+        epochs = 60, 
+        mini_batch_size = 128, 
+        eta = 0.05, #learning rate 
+        lmbda = 5.0, 
+        evaluation_data=validation_data, 
+        monitor_evaluation_accuracy=True, 
+        monitor_evaluation_cost=True, 
+        monitor_training_accuracy=False, 
+        monitor_training_cost=False, 
+        learning_schedule=True)
+print("Training done! Now saving")
+net.save("trained_model.json")
+print("Model saved successfully as 'trained_model.json'!")
